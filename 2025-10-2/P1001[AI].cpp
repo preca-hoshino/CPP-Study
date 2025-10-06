42baa9e92fc585ba87b2a612653964fc0fbec61b
preca-hoshino <001@labmem.com.cn>
2025-10-06 14:29:05 +0800

    [Add] P1001[AI]

---
+++ 2025-10-2/P1001[AI].cpp
@@ -0,0 +1,13 @@
+// AGENT CODE
+// P1001 - A+B Problem
+// GitHub Copilot - 2025-10-06 10:30:00
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
