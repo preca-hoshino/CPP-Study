e7fc0b70f719568e5aba8e2c3f2fbb1139cdaeab
preca-hoshino <001@labmem.com.cn>
2025-10-06 14:25:40 +0800

    [Add] B2023[AI]

---
+++ 2025-10-2/B2023[AI].cpp
@@ -0,0 +1,15 @@
+// AGENT CODE
+// B2023 - 空格分隔输出
+// GitHub Copilot - 2025-10-06 13:30:00
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch;
+	int n = 0;
+	float f1 = 0;
+	double f2 = 0;
+	cin >> ch >> n >> f1 >> f2;
+	cout << fixed << setprecision(6) << ch << " " << n << " " << f1 << " " << f2;
+	return 0;
+}
