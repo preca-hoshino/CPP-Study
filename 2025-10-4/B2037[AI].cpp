6294c46a0e11f04abf55917b01734c38ff42ef57
preca-hoshino <001@labmem.com.cn>
2025-10-24 00:16:55 +0800

    [Add] B2037[AI]

---
+++ 2025-10-4/B2037[AI].cpp
@@ -0,0 +1,19 @@
+// AGENT CODE
+// B2037 - 奇偶数判断
+// GitHub Copilot - 2025-10-24 00:00:00
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "even";
+    }
+    else
+    {
+        cout << "odd";
+    }
+    return 0;
+}
