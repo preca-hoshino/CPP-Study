86216e256065cae886fd058cd8c4cc5192698ee5
preca-hoshino <001@labmem.com.cn>
2025-10-24 00:19:07 +0800

    [Add] B2038[AI]

---
+++ 2025-10-4/B2038[AI].cpp
@@ -0,0 +1,19 @@
+// AGENT CODE
+// B2038 - 奇偶 ASCII 值判断
+// GitHub Copilot - 2025-10-24 00:00:00
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    cin >> c;
+    if (c % 2 == 1)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
